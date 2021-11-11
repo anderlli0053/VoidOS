fb3b3c0b5a995bbf7923f2b5461034d450d3bbdd
Andrew Poženel - SDT <anderlli0053@live.com>
2021-11-11 23:19:34 +0100

    Update
    Update - changed some things, added licenses for the used software components
    
    Co-Authored-By: Andrew Poženel - SDT <22171451+anderlli0053@users.noreply.github.com>

--- Booting_Up.gd
+++ Booting_Up.gd
@@ -11,6 +11,5 @@ extends Control
 func _on_Timer_timeout() -> void:
 
 	$".".queue_free()
-
-
-	pass # Replace with function body.
+	get_tree().change_scene("res://main.tscn")
+	print_debug("Changed active scene to main.tscn")
